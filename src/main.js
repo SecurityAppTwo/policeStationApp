4974ace56cd8ab26486f9ab61df2ce2725e81d2e
hackathon IDF <hack@cs.colman.ac.il>
2021-06-06 19:08:52 +0300

    little changes

--- src/main.js
+++ src/main.js
@@ -2,12 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
-import vuetify from './plugins/vuetify'
-
-
-Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
