8eb0c9f462ffe2a078ca939f0a7d7e12a11531d1
hackathon IDF <hack@cs.colman.ac.il>
2021-06-07 19:46:45 +0300

    Added changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
   <div id="app" style="background-color: #6495ED; height: 100vh;">
     <div class = "row">
-      <div class = "col" v-for="(button, key) in buttons" :key = "key">
+      <div class = "col">
         <button class = "center">
           <a>
-          {{ button.name }}
+          לוח פעילויות
           </a> 
         </button>
           <i style="padding-top:3vh !important" class="fas fa-calendar-week fa-7x  "  ></i>
+      </div>
+      <div class = "col">
+        <button class = "center">
+          <a>
+         מפה
+          </a> 
+        </button>
+          <i style="padding-top:3vh !important" class="fas fa-map-marked-alt fa-7x "  ></i>
+      </div>
+      <div class = "col">
+        <button class = "center">
+          <a>
+          דיווחים
+          </a> 
+        </button>
+          <i style="padding-top:3vh !important" class="fas fa-broadcast-tower fa-7x  "  ></i>
       </div>
     </div>
     <actions-page/>
@@ -25,7 +41,7 @@ export default {
   },
   data () {
     return {
-      buttons : [{name : "לוח פעילויות", omri : "fas fa-calendar-week fa-7x"},{name : "מפה", omri : "fas fa-map-marked-alt fa-7x"}, {name : "דיווחים", omri : "fas fa-broadcast-tower fa-7x"} ]
+      buttons : [{name : "לוח פעילויות", icon  : "fas fa-calendar-week fa-7x"},{name : "מפה", icon : "fas fa-map-marked-alt fa-7x"}, {name : "דיווחים", icon : "fas fa-broadcast-tower fa-7x"} ]
     }
   }
 }
@@ -33,7 +49,7 @@ export default {
 
 <style>
 #app {
-
+  /* background-image: url("https://i.pinimg.com/564x/2d/4e/f0/2d4ef0d384578ee83ccd92967f1db160.jpg");  */
 }
 
 a{
@@ -50,7 +66,7 @@ i {
   display: inline;
   text-align: center;
   justify-content: center;
-  background-color:cadetblue;
+  /* background-color:cadetblue; */
 }
 
 .col{
--- src/components/ActionsPage.vue
+++ src/components/ActionsPage.vue
@@ -1,158 +1,265 @@
 <template>
-    <div class = "ActionsPage">
-         <h1 style="color: red; front-size: 20vw; text-align: center;"> דוח פעילויות</h1>
-         <button style="color:white; background-color:blue; text-align: right; margin-left: 70vw;"><a style-="front-size: 20vw;">סינון </a></button>
-         <div class="window">
-            <div v-for="(action,key) in actions" :key=key v-on:click="openModal(action)">
-                <button class = "row">
-                <a>
-                   {{ action.description }}
-                </a>
-                <a>
-                    {{ action.date }}
-                </a>
-                 </button>
-            </div>
-             <button v-on:click="openModal2()">
-                 <i class="fas fa-plus fa-3x"></i>
-             </button>
-         </div>
-
-         <transition name="fade">
-            <div class="modal" v-if="show">
-            <div class="modal__backdrop" @click="closeModal()"/>
-
-            <div class="modal__dialog">
-                <div class="modal__header">
-                <slot name="header"/>
-                <button type="button" class="modal__close" @click="closeModal()">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
-                    <path
-                        fill="currentColor"
-                        d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
-                    ></path>
-                    </svg>
-                </button>
-                </div>
-
-                <div class="modal__body">
-                    <h2>
-                    סוג פעולה: {{ currentAction.kind }}
-                    </h2>
-                   <h2>
-                     זמן מתוכנן לפעילות: {{ currentAction.date }}
-                   </h2>
-                   <h2>
-                     כוח מתוכנן: {{ currentAction.power }}
-                   </h2> 
-                   <h2>
-                     מטרת הפעילות: {{ currentAction.target }}
-                   </h2>  
-                   <h2>
-                     אישור הפעילות: {{ currentAction.approvedBy }}
-                   </h2>  
-                   <h2>
-                     מיקום: {{ currentAction.location }}
-                   </h2>   
-                <slot name="body"/>
-                </div>
-
-                <div class="modal__footer">
-                <slot name="footer"/>
-                </div>
-            </div>
-            </div>
-        </transition>
-
-        <transition name="fade">
-            <div class="modal" v-if="show2">
-            <div class="modal__backdrop" @click="closeModal2()"/>
-
-            <div class="modal__dialog">
-                <div class="modal__header">
-                    <h1 style="color: red;">
-                        פעילות חדשה
-                    </h1>
-                <slot name="header"/>
-                <button type="button" class="modal__close" @click="closeModal2()">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
-                    <path
-                        fill="currentColor"
-                        d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
-                    ></path>
-                    </svg>
-                </button>
-                </div>
-
-                <div class="modal__body">
-                     <div>
-                        <input type="text" id="location" name="location" v-model="description" placeholder="מיקום..">
-                        <label for="fname">תיאור</label>
-                    </div>
-                    <div>
-                        <input type="text" id="kind" name="kind" v-model="kind" placeholder="סוג הפעולה..">
-                        <label for="fname">סוג הפעולה</label>
-                    </div>
-                     <div>
-                        <input type="text" id="time" name="time" v-model="time" placeholder="זמן מתכונן..">
-                        <label for="fname">זמן מתוכנן לפעילות</label>
-                    </div>
-                     <div>
-                        <input type="text" id="force" name="force" v-model="force" placeholder="כוח מתוכנן..">
-                        <label for="fname">כוח מתוכנן</label>
-                    </div>
-                     <div>
-                        <input type="text" id="target" name="target" v-model="target" placeholder="מטרת הפעילות..">
-                        <label for="fname">מטרת הפעילות</label>
-                    </div>
-                     <div>
-                        <input type="text" id="approve" name="approve" v-model="approve" placeholder="אישור הפעילות..">
-                        <label for="fname">אישור הפעילות</label>
-                    </div>
-                     <div>
-                        <input type="text" id="location" name="location" v-model="location" placeholder="מיקום..">
-                        <label for="fname">מיקום</label>
-                    </div>
-                    <button class="omri" style="background-color: green;" v-on:click="addAction(); closeModal2();">
-                        יצירת פעולה
-                    </button>
-                <slot name="body"/>
-                </div>
-
-                <div class="modal__footer">
-                <slot name="footer"/>
-                </div>
-            </div>
-            </div>
-        </transition>
+  <div class="ActionsPage">
+    <a
+      ><h3 style="color: red; front-size: 40vw; text-align: center;">
+        דוח פעילויות
+      </h3></a
+    >
+    <button
+      style="color:white; background-color:blue; text-align: right; margin-left: 70vw;"
+    >
+      <a style-="front-size: 20vw;">סינון </a>
+    </button>
+    <div class="window">
+      <button
+        class="roww"
+        v-for="(action, key) in allActivities"
+        :key="key"
+        v-on:click="openModal(action)"
+      >
+        <a class="insideButton">
+          {{ action.description }}
+        </a>
+        <a>
+          {{ action.date }}
+        </a>
+      </button>
+      <button class="nextP" v-on:click="openModal2()">
+        <i class="fas fa-plus fa-3x"></i>
+      </button>
     </div>
-        
+
+    <transition name="fade">
+      <div class="modal" v-if="show">
+        <div class="modal__backdrop" @click="closeModal()" />
+
+        <div class="modal__dialog">
+          <div class="modal__header">
+            <slot name="header" />
+            <button type="button" class="modal__close" @click="closeModal()">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                <path
+                  fill="currentColor"
+                  d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+
+          <div class="modal__body">
+            <h2>סוג פעולה: {{ currentAction.kind }}</h2>
+            <h2>זמן מתוכנן לפעילות: {{ currentAction.date }}</h2>
+            <h2>כוח מתוכנן: {{ currentAction.power }}</h2>
+            <h2>מטרת הפעילות: {{ currentAction.target }}</h2>
+            <h2>אישור הפעילות: {{ currentAction.approvedBy }}</h2>
+            <h2>מיקום: {{ currentAction.location }}</h2>
+            <slot name="body" />
+          </div>
+
+          <div class="modal__footer">
+            <slot name="footer" />
+          </div>
+        </div>
+      </div>
+    </transition>
+
+    <transition name="fade">
+      <div class="modal" v-if="show2">
+        <div class="modal__backdrop" @click="closeModal2()" />
+
+        <div class="modal__dialog">
+          <div class="modal__header">
+            <h1 style="color: red;">
+              פעילות חדשה
+            </h1>
+            <slot name="header" />
+            <button type="button" class="modal__close" @click="closeModal2()">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512">
+                <path
+                  fill="currentColor"
+                  d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+
+          <div class="modal__body">
+            <div>
+              <input
+                type="text"
+                id="location"
+                name="location"
+                v-model="description"
+                placeholder="מיקום.."
+              />
+              <label for="fname">תיאור</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="kind"
+                name="kind"
+                v-model="kind"
+                placeholder="סוג הפעולה.."
+              />
+              <label for="fname">סוג הפעולה</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="time"
+                name="time"
+                v-model="time"
+                placeholder="זמן מתכונן.."
+              />
+              <label for="fname">זמן מתוכנן לפעילות</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="force"
+                name="force"
+                v-model="force"
+                placeholder="כוח מתוכנן.."
+              />
+              <label for="fname">כוח מתוכנן</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="target"
+                name="target"
+                v-model="target"
+                placeholder="מטרת הפעילות.."
+              />
+              <label for="fname">מטרת הפעילות</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="approve"
+                name="approve"
+                v-model="approve"
+                placeholder="אישור הפעילות.."
+              />
+              <label for="fname">אישור הפעילות</label>
+            </div>
+            <div>
+              <input
+                type="text"
+                id="location"
+                name="location"
+                v-model="location"
+                placeholder="מיקום.."
+              />
+              <label for="fname">מיקום</label>
+            </div>
+            <button
+              class="omri"
+              style="background-color: green;"
+              v-on:click="
+                addAction();
+                closeModal2();
+              "
+            >
+              יצירת פעולה
+            </button>
+            <slot name="body" />
+          </div>
+
+          <div class="modal__footer">
+            <slot name="footer" />
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'actionsPage',
-  components: {
-    
-  },
-  data () {
+  name: "actionsPage",
+  components: {},
+  data() {
     return {
-      actions : [{description : "החשוד שדד זקנה ועל כך צריך להעצר", date : "27/7/2011", location: "מודיעין", kind: "מארב", power : "פרלטה, סנטיאגו ועומרי", target: "מארב לתפיסת החשוד", approvedBy: "קפטן ריי הולט" },
-      {description : "החשוד מחזיק נשק לעצור במיידי", date : "27/5/2004", location: "באר שבע", kind: "מארב", power : "פרלטה, סנטיאגו ועומרי", target: "מארב לתפיסת החשוד", approvedBy: "קפטן ריי הולט" }, 
-      {description : "החשוד נמצא בתל אביב", date : "1/2/2021", location: "תל אביב",kind: "מארב", power : "פרלטה, סנטיאגו ועומרי", target: "מארב לתפיסת החשוד", approvedBy: "קפטן ריי הולט" } ],
+      actions: [
+        {
+          description: "החשוד שדד זקנה ועל כך צריך להעצר",
+          date: "27/7/2011",
+          location: "מודיעין",
+          kind: "מארב",
+          power: "פרלטה, סנטיאגו ועומרי",
+          target: "מארב לתפיסת החשוד",
+          approvedBy: "קפטן ריי הולט"
+        },
+        {
+          description: "החשוד מחזיק נשק לעצור במיידי",
+          date: "27/5/2004",
+          location: "באר שבע",
+          kind: "מארב",
+          power: "פרלטה, סנטיאגו ועומרי",
+          target: "מארב לתפיסת החשוד",
+          approvedBy: "קפטן ריי הולט"
+        },
+        {
+          description: "החשוד נמצא בתל אביב",
+          date: "1/2/2021",
+          location: "תל אביב",
+          kind: "מארב",
+          power: "פרלטה, סנטיאגו ועומרי",
+          target: "מארב לתפיסת החשוד",
+          approvedBy: "קפטן ריי הולט"
+        }
+      ],
       show: false,
       show2: false,
-      location: '',
-      approve: '',
-      target: '',
-      force: '',
-      time: '',
-      kind: '',
-      description: '',
-      currentAction: null
-    }
+      location: "",
+      approve: "",
+      target: "",
+      force: "",
+      time: "",
+      kind: "",
+      description: "",
+      currentAction: null,
+      allActivities: null,
+      allUsers: null,
+      allCops: null
+    };
   },
-   methods: {
+  async created() {
+    await axios
+      .get("http://localhost:8081/activities/all")
+      .then(response => {
+        this.allActivities = response.data;
+      })
+      .catch(e => {
+        throw e;
+      });
+
+    console.log(this.allActivities);
+    console.log(1);
+
+    await axios
+      .get("http://localhost:8081/activities/users")
+      .then(response => {
+        this.allUsers = response.data;
+      })
+      .catch(e => {
+        throw e;
+      });
+
+    await axios
+      .get("http://localhost:8081/activities/cops")
+      .then(response => {
+        this.allCops = response.data;
+      })
+      .catch(e => {
+        throw e;
+      });
+  },
+  methods: {
     closeModal() {
       this.show = false;
       document.querySelector("body").classList.remove("overflow-hidden");
@@ -170,26 +277,33 @@ export default {
       this.show2 = true;
       document.querySelector("body").classList.add("overflow-hidden");
     },
-    addAction(){
-        console.log(this.actions)
-        this.actions.push( {description : this.description , date: this.time, location: this.location, kind: this.kind, power: this.force, target: this.target, approvedBy: this.approve})
-        console.log(this.actions)
+    addAction() {
+      console.log(this.actions);
+      this.actions.push({
+        description: this.description,
+        date: this.time,
+        location: this.location,
+        kind: this.kind,
+        power: this.force,
+        target: this.target,
+        approvedBy: this.approve
+      });
+      console.log(this.actions);
     }
-}
-}
+  }
+};
 </script>
 
 <style>
+.roww {
+  width: 100%;
+}
 
-.row {
-  display: flex;
-  flex-flow: row nowrap;
-  width: 50% ;
+.nextP {
 }
 
 .omri {
-   
-  background-color: #4CAF50; /* Green */
+  background-color: #4caf50; /* Green */
   border: none;
   color: white;
   padding: 15px 32px;
@@ -197,15 +311,13 @@ export default {
   text-decoration: none;
   display: inline-block;
   font-size: 16px;
-
 }
 
 .window {
-    background-color: wheat;
-    height: 50vh;
-    width:80%;
-    margin-left: 10%;
-    
+  background-color: wheat;
+  height: 46vh;
+  width: 71%;
+  margin-left: 10%;
 }
 .modal {
   overflow-x: hidden;
@@ -217,47 +329,47 @@ export default {
   left: 0;
   z-index: 9;
 }
-  .modal__backdrop {
-    background-color: rgba(0, 0, 0, 0.3);
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: 1;
-  }
-  .modal__dialog {
-    background-color: #ffffff;
-    position: relative;
-    width: 600px;
-    margin: 50px auto;
-    display: flex;
-    flex-direction: column;
-    border-radius: 5px;
-    z-index: 2;
-  }
-  .modal__close {
-    width: 30px;
-    height: 30px;
-  }
-  .modal__header {
-    padding: 20px 20px 10px;
-    display: flex;
-    align-items: flex-start;
-    justify-content: space-between;
-  }
-  .modal__body {
-    padding: 10px 20px 10px;
-    overflow: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    text-align: right;
-    flex: right;    
-  }
-  .modal__footer {
-    padding: 10px 20px 20px;
-  }
+.modal__backdrop {
+  background-color: rgba(0, 0, 0, 0.3);
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+}
+.modal__dialog {
+  background-color: #ffffff;
+  position: relative;
+  width: 600px;
+  margin: 50px auto;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  z-index: 2;
+}
+.modal__close {
+  width: 30px;
+  height: 30px;
+}
+.modal__header {
+  padding: 20px 20px 10px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+.modal__body {
+  padding: 10px 20px 10px;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  text-align: right;
+  flex: right;
+}
+.modal__footer {
+  padding: 10px 20px 20px;
+}
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.2s;
