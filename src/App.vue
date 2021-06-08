3e56d28003ce29b458945eb9e44d0b023b42c463
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 17:59:20 +0300

    added map page

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,45 @@
 <template>
   <div id="app" style="background-color: #75899f">
-    <div class = "row">
-      <div class = "col" v-for="(button, key) in buttons" :key = "key">
-        <button class = "center">
+    <div class="row">
+      <div class="col" v-for="(button, key) in buttons" :key="key">
+        <button class="center">
           <a>
-          {{ button.name }}
-          </a> 
+            {{ button.name }}
+          </a>
         </button>
-          <i class="fas fa-calendar-week fa-7x  "  ></i>
+        <i class="fas fa-calendar-week fa-7x  "></i>
       </div>
     </div>
-    <actions-page/>
+    <map-page />
   </div>
 </template>
 
 <script>
-
 // import HelloWorld from './components/HelloWorld.vue'
-import ActionsPage from './components/ActionsPage.vue'
+// import ActionsPage from './components/ActionsPage.vue'
+import MapPage from "./components/MapPage.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    ActionsPage
+    MapPage
   },
-  data () {
+  data() {
     return {
-      buttons : [{name : "לוח פעילויות", omri : "fas fa-calendar-week fa-7x"},{name : "מפה", omri : "fas fa-map-marked-alt fa-7x"}, {name : "דיווחים", omri : "fas fa-broadcast-tower fa-7x"} ]
-    }
+      buttons: [
+        { name: "לוח פעילויות", omri: "fas fa-calendar-week fa-7x" },
+        { name: "מפה", omri: "fas fa-map-marked-alt fa-7x" },
+        { name: "דיווחים", omri: "fas fa-broadcast-tower fa-7x" }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
 }
 
-a{
+a {
   font-size: 3vw;
 }
 
@@ -49,18 +53,17 @@ i {
   display: inline;
   text-align: center;
   justify-content: center;
-  background-color:cadetblue;
+  background-color: cadetblue;
 }
 
-.col{
-   flex-flow: column nowrap;
-   justify-content: space-between;
-    display: flex;
-
+.col {
+  flex-flow: column nowrap;
+  justify-content: space-between;
+  display: flex;
 }
 
 .row {
-width: 40vw;
+  width: 40vw;
   display: flex;
   /* text-align: center; */
   flex-flow: row nowrap;
---
+++ src/components/MapPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <iframe
+      :src="`http://alpha-maps-git-tmzmap2.apps.openforce.openforce.biz/`"
+      width="100%"
+      height="100"
+      frameborder="0"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
