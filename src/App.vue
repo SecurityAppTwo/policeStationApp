26012eeb06e6931d3cee65c90973ad4760e74843
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 19:11:11 +0300

    added updated map page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
+Skip to content Search or jump to… Pull requests Issues Marketplace Explore
+@mayarub SecurityAppTwo / policeStationApp 0 0 0 Code Issues Pull requests
+Actions Projects Wiki Security 1 Insights Settings policeStationApp/src/App.vue
+hackathon IDF changed styles and db connection Latest commit 11281b5 8 hours ago
+History 0 contributors We found potential security vulnerabilities in your
+dependencies. You can see this message because you have been granted access to
+Dependabot alerts for this repository. 99 lines (91 sloc) 1.96 KB
+
 <template>
-  <div id="app" style="background-color: #75899f">
+  <div id="app" style="background-color: #caf0f8; height: 100vh;">
     <div class="row">
-      <div class="col" v-for="(button, key) in buttons" :key="key">
+      <div class="col">
         <button class="center">
           <a>
-            {{ button.name }}
+            לוח פעילויות
           </a>
         </button>
-        <i class="fas fa-calendar-week fa-7x  "></i>
+        <i
+          style="padding-top:3vh !important"
+          class="fas fa-calendar-week fa-7x  "
+        ></i>
+      </div>
+      <div class="col">
+        <button class="center">
+          <a>
+            מפה
+          </a>
+        </button>
+        <i
+          style="padding-top:3vh !important"
+          class="fas fa-map-marked-alt fa-7x "
+        ></i>
+      </div>
+      <div class="col">
+        <button class="center">
+          <a>
+            דיווחים
+          </a>
+        </button>
+        <i
+          style="padding-top:3vh !important"
+          class="fas fa-broadcast-tower fa-7x  "
+        ></i>
       </div>
     </div>
     <map-page />
@@ -16,7 +49,7 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-// import ActionsPage from './components/ActionsPage.vue'
+// import ActionsPage from "./components/ActionsPage.vue";
 import MapPage from "./components/MapPage.vue";
 export default {
   name: "App",
@@ -26,9 +59,9 @@ export default {
   data() {
     return {
       buttons: [
-        { name: "לוח פעילויות", omri: "fas fa-calendar-week fa-7x" },
-        { name: "מפה", omri: "fas fa-map-marked-alt fa-7x" },
-        { name: "דיווחים", omri: "fas fa-broadcast-tower fa-7x" }
+        { name: "לוח פעילויות", icon: "fas fa-calendar-week fa-7x" },
+        { name: "מפה", icon: "fas fa-map-marked-alt fa-7x" },
+        { name: "דיווחים", icon: "fas fa-broadcast-tower fa-7x" }
       ]
     };
   }
@@ -37,31 +70,27 @@ export default {
 
 <style>
 #app {
+  /* background-image: url("https://i.pinimg.com/564x/2d/4e/f0/2d4ef0d384578ee83ccd92967f1db160.jpg");  */
 }
-
 a {
   font-size: 3vw;
 }
-
 i {
   width: 80px !important;
   height: 80px !important;
   size: 40px;
 }
-
 .center {
   display: inline;
   text-align: center;
   justify-content: center;
-  background-color: cadetblue;
+  /* background-color:cadetblue; */
 }
-
 .col {
   flex-flow: column nowrap;
   justify-content: space-between;
   display: flex;
 }
-
 .row {
   width: 40vw;
   display: flex;
--- src/components/MapPage.vue
+++ src/components/MapPage.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
-    <iframe
-      :src="`http://alpha-maps-git-tmzmap2.apps.openforce.openforce.biz/`"
-      width="100%"
-      height="100"
-      frameborder="0"
-    >
-    </iframe>
+    <div class="map">
+      <iframe
+        :src="`http://alpha-maps-git-tmzmap2.apps.openforce.openforce.biz/`"
+        width="100%"
+        height="100"
+        frameborder="0"
+      >
+      </iframe>
+    </div>
   </div>
 </template>
 
@@ -14,4 +16,13 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.map {
+  margin-top: 300px;
+  margin-bottom: 200px;
+  width: 50vw;
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+}
+</style>
