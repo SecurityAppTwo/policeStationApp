cb9f9d775ec90355c5ac93d33424a5f701862085
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 19:54:12 +0300

    fix (all files): fix design

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
-  <div id="app" style="background-color: #caf0f8; height: 100vh;">
+  <div id="app" style="background-color: #5C677D; height: 100vh;">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-// import LoginPage from "./components/LoginPage.vue";
-// import ActionsPage from "./components/ActionsPage.vue";
-// import Navbar from "./components/Navbar.vue";
-
 export default {
   name: "App",
-  components: {
-    // Navbar
-    // LoginPage
-  },
+  components: {},
   data() {
     return {};
   }
